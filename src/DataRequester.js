6ca60776354fc155b1012e11e6d988c01c8f269e
Sam King <samking419@gmail.com>
2023-04-18 17:03:11 -0400

    API Requests via Amplify-given syntax

--- src/DataRequester.js
+++ src/DataRequester.js
@@ -1,3 +1,8 @@
+import { DataStore } from '@aws-amplify/datastore';
+import { Dorm } from './models';
+import { Review } from './models';
+import { Account } from './models';
+
 const url = ''; //API endpoint URL
 
 //Super basic get request
@@ -26,3 +31,62 @@ need to serialize into a JSON object.
 I believe the basic idea is that we will have to pass our data
 from the methods they've made into here and work from there.
 It shouldn't be too bad but it will take time to do right*/
+
+
+//This section is for Amplify defined API calls
+
+// This is a GET request. We pass in a DataType to choose where our get comes from
+// If we don't the method will fail
+async function getData(dataType){
+    try{
+        // Need to test to ensure ENUMS work as expected
+        const models = await DataStore.query(dataType);
+        console.log(models);
+    } catch (Error){
+        console.log(Error);
+    }
+}
+
+//This is a POST request. We pass in a DataType to choose where our post goes to
+//If we don't the method will fail
+async function postData(dataType, JsonObject){
+    await DataStore.save(
+        new dataType({
+            JsonObject
+        })
+    );
+}
+
+//Ex JsonObject:
+/*
+    new Dorm({
+		"Name": "Lorem ipsum dolor sit amet",
+		"Year": "Lorem ipsum dolor sit amet",
+		"NumResidents": NumPeople.ONE,
+		"NumBathrooms": 1020,
+		"Kitchen": true,
+		"Refrigerator": true,
+		"Microwave": true,
+		"AirConditioning": true,
+		"CommunityBathroom": true,
+		"CommunityMicrowave": true,
+		"CommunityKitchen": true,
+		"Community": Community.CEDARMAGNOLIA
+	})
+*/
+
+
+//This is a DELETE request.
+async function deleteData(dataType, dataId){
+    const modelToDelete = await DataStore.query(dataType, dataId);
+    DataStore.delete(modelToDelete);
+}
+
+//This is a PUT request (i.e. an update request)
+async function updateData(dataType, CURRENT_ITEM){
+    /* Models in DataStore are immutable. To update a record you must use the copyOf function
+ to apply updates to the item’s fields rather than mutating the instance directly */
+    await DataStore.save(datType.copyOf(CURRENT_ITEM, item => {
+        // Update the values on {item} variable to update DataStore entry
+    }));
+}
