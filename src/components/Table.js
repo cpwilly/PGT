075fdfd8571654c5f9100b53ef813ec69c9e64bb
Charlie Lin <clin10900@gmail.com>
2023-04-21 00:34:52 -0400

    Style changes to review component

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,8 +4,7 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Review from './Review';
+import Review from './review';
 
 function createData(review) {
   return { review };
